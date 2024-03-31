11d0b24fa5df68f8cc85379d0ae120266dab4a99
miriam <miriamhuertahernandez2018@gmail.com>
2024-03-30 21:20:19 -0600

    bucles

---
+++ app.js
@@ -0,0 +1,16 @@
+
+
+//Bucle while
+let i = 1;
+while (i <= 10) {
+    document. write("El valor del bucle es: " + i + "<br>")
+    i++;
+}
+
+
+//Bucle do while
+let i = 0;
+do {
+    document.write("El valor de i es: " + i + "<br>");
+    i+=5;
+} while (i <= 50);
